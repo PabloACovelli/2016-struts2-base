9297c5c718505ba75a3d981398e1c5f6d772dbeb
Pablo Covelli <pabloacovelli@gmail.com>
2016-04-13 14:33:26 -0300

    Modificado

--- src/utn/frd/action/PersonaAction.java
+++ src/utn/frd/action/PersonaAction.java
@@ -16,56 +16,9 @@ public class PersonaAction extends ActionSupport {
 	private String name;
 	private String age;
 	private String gender;
-	private String id;
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
 	private List<Persona>personas;
+
 	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public List<Persona> getPersonas() {
-		return personas;
-	}
-
-	public void setPersonas(List<Persona> personas) {
-		this.personas = personas;
-	}
-	
-	public String execute(){//inicializar coleccion
-		personas = PersistentManager.getInstance();
-		
-		return SUCCESS;
-	}
 	
 	
 	public String save(){
@@ -74,21 +27,18 @@ public class PersonaAction extends ActionSupport {
 		int edad = 0;
 		
 		try{
-			Persona p= new Persona(personas.size(), name, edad, gender);
-			personas.add(p);
-		edad = Integer.parseInt(age);
+			edad = Integer.parseInt(age);
 		}
 		catch(Exception e){
 		addActionError("Ocurrió un error con la edad");
 		return ERROR;
 		}
 		
-<<<<<<< HEAD
+
 		Persona p= new Persona(personas.size(), name, edad, gender);
 		personas.add(p);
 		
-=======
->>>>>>> origin/master
+
 		return SUCCESS;
 		
 		}
