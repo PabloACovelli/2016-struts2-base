32bc2ef51df8acc559082a68a66f34c5e85cc2d5
Brian92102 <brian92102@gmail.com>
2016-04-12 23:55:31 -0300

    TP
    ejercicio de struts

--- src/utn/frd/action/PersonaAction.java
+++ src/utn/frd/action/PersonaAction.java
@@ -16,13 +16,64 @@ public class PersonaAction extends ActionSupport {
 	private String name;
 	private String age;
 	private String gender;
-	private List<Persona> personas;
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	private List<Persona>personas;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public List<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(List<Persona> personas) {
+		this.personas = personas;
+	}
+	
+	public String execute(){//inicializar coleccion
+		personas = PersistentManager.getInstance();
+		
+		return SUCCESS;
+	}
 	
 	public String save(){
 		
 		personas = PersistentManager.getInstance();
 		int edad = 0;
 		try{
+			Persona p= new Persona(personas.size(), name, edad, gender);
+			personas.add(p);
 		edad = Integer.parseInt(age);
 		}
 		catch(Exception e){
@@ -30,8 +81,6 @@ public class PersonaAction extends ActionSupport {
 		return ERROR;
 		}
 		
-		Persona p= new Persona(personas.size(), name, edad, gender);
-		personas.add(p);
 		return SUCCESS;
 		
 		}
